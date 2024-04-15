acf475a0035d7b1d1b27070783ed72e3a7f38296
Ítalo Gustavo <42496107+ItaloGustavoS@users.noreply.github.com>
2024-04-15 10:27:25 -0300

    improve

--- black/src/type.js
+++ black/src/type.js
@@ -7,259 +7,147 @@
  ******************************************************************************/
 
 /// Module type ////////////////////////////////////////////////////////////////
-void function (root) {
+type Module = {
+  class_of: (obj: any) => string;
+  nilp: (obj: any) => boolean;
+  not_nilp: (obj: any) => boolean;
+  undefp: (obj: any) => boolean;
+  strp: (obj: any) => boolean;
+  nump: (obj: any) => boolean;
+  regexpp: (obj: any) => boolean;
+  fnp: (obj: any) => boolean;
+  boolp: (obj: any) => boolean;
+  objp: (obj: any) => boolean;
+  listp: (obj: any) => boolean;
+  callablep: (obj: any) => boolean;
+  numericp: (obj: any) => boolean;
+  sequencep: (obj: any) => boolean;
+  sliceablep: (obj: any) => boolean;
+  searchablep: (obj: any) => boolean;
+  testablep: (obj: any) => boolean;
+  $black_box: any;
+  $black_utils: {
+    class_of: (obj: any) => string;
+    nilp: (obj: any) => boolean;
+    not_nilp: (obj: any) => boolean;
+    undefp: (obj: any) => boolean;
+    strp: (obj: any) => boolean;
+    nump: (obj: any) => boolean;
+    regexpp: (obj: any) => boolean;
+    fnp: (obj: any) => boolean;
+    boolp: (obj: any) => boolean;
+    objp: (obj: any) => boolean;
+    listp: (obj: any) => boolean;
+    callablep: (obj: any) => boolean;
+    numericp: (obj: any) => boolean;
+    sliceablep: (obj: any) => boolean;
+    searchablep: (obj: any) => boolean;
+    testablep: (obj: any) => boolean;
+  };
+};
 
-    var type
+function class_of(obj: any): string {
+  return Object.prototype.toString.call(obj).slice(8, -1);
+}
 
-      , __class = Object.prototype.toString
+function nilp(obj: any): boolean {
+  return obj == null;
+}
 
-
-    //// -Type checking ////////////////////////////////////////////////////////
+function not_nilp(obj: any): boolean {
+  return obj != null;
+}
 
-    ///// Function class_of ////////////////////////////////////////////////////
-    //
-    //   (obj:Obj) ↦ String
-    //
-    // Returns the internal `[[Class]]` of the given object.
-    //
-    function class_of(obj) {
-        return __class.call(obj).slice(8, -1)
-    }
+function undefp(obj: any): boolean {
+  return obj === void 0;
+}
 
-    ///// Function nilp ////////////////////////////////////////////////////////
-    //
-    //   (obj:Obj) ↦ Bool
-    //
-    // Checks if the object does not point to anything valid.
-    //
-    // That is, whether something is `null` or `undefined`.
-    //
-    function nilp(obj) {
-        return obj == null
-    }
+function strp(obj: any): boolean {
+  return class_of(obj) === "String";
+}
 
-    ///// Function not_nilp ////////////////////////////////////////////////////
-    //
-    //   (obj:Obj) ↦ Bool
-    //
-    // Checks if the object points to anything valid.
-    //
-    // That is, whether something is not `null` nor `undefined`.
-    //
-    function not_nilp(obj) {
-        return obj != null
-    }
+function nump(obj: any): boolean {
+  return class_of(obj) === "Number";
+}
 
-    ///// Function undefp //////////////////////////////////////////////////////
-    //
-    //   (obj:Obj) ↦ Bool
-    //
-    // Checks if the object is undefined.
-    //
-    function undefp(obj) {
-        return obj === void 0
-    }
+function regexpp(obj: any): boolean {
+  return class_of(obj) === "RegExp";
+}
 
-    ///// Function strp ////////////////////////////////////////////////////////
-    //
-    //   (obj:Obj) ↦ Bool
-    //
-    // Checks if the object is a string.
-    //
-    function strp(obj) {
-        return class_of(obj) == 'String'
-    }
+function fnp(obj: any): boolean {
+  return class_of(obj) === "Function";
+}
 
-    ///// Function nump ////////////////////////////////////////////////////////
-    //
-    //   (obj:Obj) ↦ Bool
-    //
-    // Checks if the object is a number.
-    //
-    function nump(obj) {
-        return class_of(obj) == 'Number'
-    }
+function boolp(obj: any): boolean {
+  return class_of(obj) === "Boolean";
+}
 
-    ///// Function regexpp /////////////////////////////////////////////////////
-    //
-    //   (obj:Obj) ↦ Bool
-    //
-    // Checks if the object is a regular expression.
-    //
-    function regexpp(obj) {
-        return class_of(obj) == 'RegExp'
-    }
+function objp(obj: any): boolean {
+  return obj === Object(obj);
+}
 
-    ///// Function fnp /////////////////////////////////////////////////////////
-    //
-    //   (obj:Obj) ↦ Bool
-    //
-    // Checks if the object is a function.
-    //
-    function fnp(obj) {
-        return class_of(obj) == 'Function'
-    }
+function listp(obj: any): boolean {
+  return obj instanceof Array;
+}
 
-    ///// Function boolp ///////////////////////////////////////////////////////
-    //
-    //   (obj:Obj) ↦ Bool
-    //
-    // Checks if the object is a boolean.
-    //
-    function boolp(obj) {
-        return class_of(obj) == 'Boolean'
-    }
+function callablep(obj: any): boolean {
+  return typeof obj === "function";
+}
 
-    ///// Function objp ////////////////////////////////////////////////////////
-    //
-    //   (obj:Obj) ↦ Bool
-    //
-    // Checks if the object is an actual object.
-    //
-    // In other words, it checks if the object is not a primitive or an
-    // invalid value.
-    //
-    function objp(obj) {
-        return obj === Object(obj)
-    }
+function numericp(obj: any): boolean {
+  return !isNaN(obj);
+}
 
-    ///// Function listp ///////////////////////////////////////////////////////
-    //
-    //   (obj:Obj) ↦ Bool
-    //
-    // Checks if the object is an actual array.
-    //
-    // :alias: Array.isArray
+function sequencep(obj: any): boolean {
+  return not_nilp(obj) && typeof obj.length === "number";
+}
 
+function sliceablep(obj: any): boolean {
+  return not_nilp(obj) && typeof obj.slice === "function";
+}
 
-
-    //// -Interface testing ////////////////////////////////////////////////////
+function searchablep(obj: any): boolean {
+  return (
+    not_nilp(obj) &&
+    typeof obj.indexOf === "function" &&
+    typeof obj.lastIndexOf === "function"
+  );
+}
 
-    ///// Function callablep ///////////////////////////////////////////////////
-    //
-    //   (obj:Obj) ↦ Bool
-    //
-    // Checks if the object can be used as a function.
-    //
-    function callablep(obj) {
-        return typeof obj == 'function'
-    }
+function testablep(obj: any): boolean {
+  return not_nilp(obj) && typeof obj.test === "function";
+}
 
-    ///// Function numericp ////////////////////////////////////////////////////
-    //
-    //   (obj:Obj) ↦ Bool
-    //
-    // Checks if the object can be used as a number.
-    //
-    // A numeric object is something that does not evaluate to `NaN`, so
-    // a string that holds a numeric value, and a list that holds a
-    // single numeric element are considered numeric.
-    //
-    function numericp(obj) {
-        return !isNaN(obj)
-    }
+/// Exports //////////////////////////////////////////////////////////////
+const root =
+  typeof globalThis !== "undefined"
+    ? globalThis
+    : typeof self !== "undefined"
+    ? self
+    : typeof window !== "undefined"
+    ? window
+    : {};
 
-    ///// Function sequencep ///////////////////////////////////////////////////
-    //
-    //   (obj:Obj) ↦ Bool
-    //
-    // Checks if the object can be used as an array.
-    //
-    // A sequence object is something that implements a `length`
-    // property, but not necessarily the methods you'd see in
-    // Array.prototype.
-    //
-    // As such, things like the `arguments` object and `HTMLCollection`
-    // are considered sequences.
-    //
-    function sequencep(obj) {
-        return not_nilp(obj)
-        && typeof obj.length == 'number'
-    }
+let type: Module;
+if (typeof exports === "undefined") {
+  type = root.black.type;
+} else {
+  type = exports;
+}
 
-
-    //// -Functionality testing ////////////////////////////////////////////////
-
-    ///// Function sliceablep //////////////////////////////////////////////////
-    //
-    //   (obj:Obj) ↦ Bool
-    //
-    // Checks if the object implements a `slice` method.
-    //
-    function sliceablep(obj) {
-        return not_nilp(obj)
-        && typeof obj.slice == 'function'
-    }
-
-    ///// Function searchablep /////////////////////////////////////////////////
-    //
-    //   (obj:Obj) ↦ Bool
-    //
-    // Checks if the object implements search methods that return indexes.
-    //
-    // Basically, we look for things that can be searched through the
-    // `indexOf` and `lastIndexOf` methods.
-    //
-    // Arrays and Strings are considered searchable.
-    //
-    function searchablep(obj) {
-        return not_nilp(obj)
-        && typeof obj.indexOf     == 'function'
-        && typeof obj.lastIndexOf == 'function'
-    }
-
-    ///// Function testablep ///////////////////////////////////////////////////
-    //
-    //   (obj:Obj) ↦ Bool
-    //
-    // Checks if the object implements a `test` method.
-    //
-    // A `test` method should take a regexp and return a boolean.
-    //
-    function testablep(obj) {
-        return not_nilp(obj)
-        && typeof obj.test == 'function'
-    }
-
-
-    ///// Exports //////////////////////////////////////////////////////////////
-    type = typeof exports == 'undefined'?  root.black.type
-                                        :  exports
-
-    type.class_of     = class_of
-    type.nilp         = nilp
-    type.not_nilp     = not_nilp
-    type.undefp       = undefp
-    type.strp         = strp
-    type.nump         = nump
-    type.regexpp      = regexpp
-    type.fnp          = fnp
-    type.boolp        = boolp
-    type.objp         = objp
-    type.listp        = Array.isArray
-    type.callablep    = callablep
-    type.numericp     = numericp
-    type.sequencep    = sequencep
-    type.sliceablep   = sliceablep
-    type.searchablep  = searchablep
-    type.testablep    = testablep
-
-    type.$black_box   = Object
-    type.$black_utils = { class_of     : class_of
-                        , nilp         : nilp
-                        , not_nilp     : not_nilp
-                        , undefp       : undefp
-                        , strp         : strp
-                        , nump         : nump
-                        , regexpp      : regexpp
-                        , fnp          : fnp
-                        , boolp        : boolp
-                        , objp         : objp
-                        , listp        : Array.isArray
-                        , callablep    : callablep
-                        , numericp     : numericp
-                        , sliceablep   : sliceablep
-                        , searcheablep : searchablep
-                        , testablep    : testablep }
-                          
-}(this)
+type.class_of = class_of;
+type.nilp = nilp;
+type.not_nilp = not_nilp;
+type.undefp = undefp;
+type.strp = strp;
+type.nump = nump;
+type.regexpp = regexpp;
+type.fnp = fnp;
+type.boolp = boolp;
+type.objp = objp;
+type.listp = listp;
+type.callablep = callablep;
+type.numericp = numericp;
+type.sequencep = sequencep;
+type.sliceablep = sliceablep;
+type
