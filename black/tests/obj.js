46231c02a76d9dcccc0ffa49b126276c936f5b75
Ítalo Gustavo <42496107+ItaloGustavoS@users.noreply.github.com>
2024-04-15 10:27:17 -0300

    improve

--- black/tests/obj.js
+++ black/tests/obj.js
@@ -1,105 +1,96 @@
-////////////////////////////////////////////////////////////////////////////////
-// Unit tests for black.obj module
-//
+// src/obj.js
 
-obj = require('../src/obj')
-require('claire')
-test = claire.test
-foo = {bar:'baz'}
-bar = Object.create(foo)
-bar.baz = 'foo'
-
-
-test('obj:: hasp -> Bool', function() {
-    var hasp = obj.hasp
-
-    assert(hasp(foo, 'bar'))
-    assert(hasp(bar, 'baz'))
-    refute(hasp(bar, 'bar'))    
-})
-
-test('obj:: emptyp -> Bool', function() {
-    var emptyp = obj.emptyp
-    
-    refute(emptyp(foo))
-    refute(emptyp(bar))
-    assert(emptyp({}))
-    assert(emptyp(Object.create(bar)))
-    assert(emptyp(Object.create(null)))           
-})
-
-test('obj:: size -> Bool', function() {
-    var size = obj.size
-
-    assert(size(foo) <eq> 1)
-    assert(size(bar) <eq> 1)
-    assert(size({}) <eq> 0)
-    assert(size(Object.create(foo)) <eq> 0)
-})
-
-test('obj:: values -> Array', function() {
-    var values = obj.values
-
-    assert(values(foo) <eq> ['baz'])
-    assert(values(bar) <eq> ['foo'])
-    assert(values({}) <eq> [])
-    assert(values(Object.create(foo)) <eq> [])
-})
-
-test('obj:: items -> Array', function() {
-    var items = obj.items
-    var baz = Object.create(foo)
-    baz.bar = 'foo'
-
-    assert(items(foo) <eq> [['bar','baz']])
-    assert(items(bar) <eq> [['baz','foo']])
-    assert(items({}) <eq> [])
-    assert(items(baz) <eq> [['bar','foo']])
-})
-
-test('obj:: get -> *mixed*', function() {
-    var get = obj.get
-    var ownp = obj.hasp
-
-    assert(get(foo, 'bar') <eq> 'baz')
-    assert(get(foo, 'baz') <eq> undefined)
-    assert(get(foo, 'baz', 'baz') <eq> 'baz')
-    assert(get(foo, 'bar', 'baz', Array.isArray) <eq> 'baz')
-
-    assert(get(bar, 'baz') <eq> 'foo')
-    assert(get(bar, 'bar') <eq> 'baz')
-    assert(get(bar, 'bar', 'foo', ownp) <eq> 'foo')
-    assert(get(bar, 'bar', 'foo', isNaN) <eq> 'baz')
-})
-
-test('obj:: set-default -> *mixed*', function() {
-    var set_default = obj.set_default
-
-    assert(set_default(foo, 'bar', 'foo') <eq> 'baz')
-    assert(set_default(foo, 'bar', 'foo', Array.isArray) <eq> 'foo')
-    assert(set_default(bar, 'bar', 'bar') <eq> 'bar')
-   
-    delete bar['bar']
-    foo['bar'] = 'baz'
-})
-
-test('obj:: nextend -> Obj', function() {
-    var nextend = obj.nextend
-
-    assert(nextend(foo, {foo:'bar'}) <eq> {foo:'bar', bar:'baz'})
-    assert(bar.foo <eq> 'bar')
-    
-    nextend({}, foo, bar)
-    refute('baz' in foo)
-})
-
-test('obj:: extend -> Obj', function() {
-    var extend = obj.extend
-
-    extend(foo, bar)
-    refute('baz' in foo)
-})
+module.exports = {
+  hasp: function(obj, key) {
+    return Object.prototype.hasOwnProperty.call(obj, key);
+  },
+  emptyp: function(obj) {
+    return Object.keys(obj).length === 0;
+  },
+  size: function(obj) {
+    return Object.keys(obj).length;
+  },
+  values: function(obj) {
+    return Object.values(obj);
+  },
+  items: function(obj) {
+    return Object.entries(obj);
+  },
+  get: function(obj, key, defaultValue) {
+    return obj.hasOwnProperty(key) ? obj[key] : defaultValue;
+  },
+  set_default: function(obj, key, defaultValue) {
+    if (!obj.hasOwnProperty(key)) {
+      obj[key] = defaultValue;
+    }
+    return obj[key];
+  },
+  nextend: function(obj, props) {
+    const newObj = Object.assign({}, obj, props);
+    Object.keys(obj).forEach(key => {
+      if (props.hasOwnProperty(key)) {
+        obj[key] = props[key];
+      }
+    });
+    return newObj;
+  },
+  extend: function(target, source) {
+    Object.keys(source).forEach(key => {
+      if (!target.hasOwnProperty(key)) {
+        target[key] = source[key];
+      }
+    });
+  },
+};
 
 
+// test/obj.spec.js
 
-claire.run()
+const obj = require('../src/obj');
+const assert = require('assert');
+
+describe('obj module', function() {
+  let foo, bar;
+
+  beforeEach(function() {
+    foo = { bar: 'baz' };
+    bar = Object.create(foo);
+    bar.baz = 'foo';
+  });
+
+  afterEach(function() {
+    delete global.foo;
+    delete global.bar;
+  });
+
+  describe('hasp', function() {
+    it('should return true if the object has the given property', function() {
+      assert(obj.hasp(foo, 'bar'));
+      assert(obj.hasp(bar, 'baz'));
+      assert(!obj.hasp(bar, 'bar'));
+    });
+  });
+
+  describe('emptyp', function() {
+    it('should return true if the object has no properties', function() {
+      assert(!obj.emptyp(foo));
+      assert(!obj.emptyp(bar));
+      assert(obj.emptyp({}));
+      assert(obj.emptyp(Object.create(null)));
+    });
+  });
+
+  describe('size', function() {
+    it('should return the number of properties in the object', function() {
+      assert(obj.size(foo) === 1);
+      assert(obj.size(bar) === 1);
+      assert(obj.size({}) === 0);
+      assert(obj.size(Object.create(foo)) === 0);
+    });
+  });
+
+  describe('values', function() {
+    it('should return an array of the object values', function() {
+      assert(obj.values(foo).includes('baz'));
+      assert(obj.values(bar).includes('foo'));
+      assert(obj.values({})
