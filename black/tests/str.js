5777fdd25b3f6417226625677611d7323ced1a75
Ítalo Gustavo <42496107+ItaloGustavoS@users.noreply.github.com>
2024-04-15 17:55:35 -0300

    improve

--- black/tests/str.js
+++ black/tests/str.js
@@ -1,20 +1,17 @@
-////////////////////////////////////////////////////////////////////////////////
-// Unit tests for the black.str module
-//
+// Unit tests for the str module
 
 const str = require('../src/str')
-const claire = require('claire')
-const test = claire.test
-const describe = claire.describe
-const assert = claire.assert
-const refute = claire.refute
-const beforeEach = claire.beforeEach
+const test = require('claire/test')
+const describe = require('claire/describe')
+const assert = require('claire/assert')
+const refute = require('claire/refute')
+const beforeEach = require('claire/beforeEach')
 
-describe('str module', function() {
+describe('str module', () => {
   let makeStr, cat, capitalise, startsWithp, endsWithp, hasp, count, dasherise, camelise
 
-  beforeEach(function() {
-    makeStr = str.make_str
+  beforeEach(() => {
+    makeStr = str.makeStr
     cat = str.cat
     capitalise = str.capitalise
     startsWithp = str.starts_withp
@@ -25,28 +22,33 @@ describe('str module', function() {
     camelise = str.camelise
   })
 
-  test('make_str function', function() {
-    assert(makeStr('foo') === '', 'make_str("foo") should return ""')
-    assert(makeStr('foo', 2) === 'foofoo', 'make_str("foo", 2) should return "foofoo"')
-    assert(makeStr('foo', 1) === 'foo', 'make_str("foo", 1) should return "foo"')
-    assert(makeStr('foo', -1) === '', 'make_str("foo", -1) should return ""')
-    assert(makeStr('foo', 0) === '', 'make_str("foo", 0) should return ""')
+  test('makeStr function', () => {
+    assert(makeStr('foo') === '', 'makeStr("foo") should return ""')
+    assert(makeStr('foo', 2) === 'foofoo', 'makeStr("foo", 2) should return "foofoo"')
+    assert(makeStr('foo', 1) === 'foo', 'makeStr("foo", 1) should return "foo"')
+    assert(makeStr('foo', -1) === '', 'makeStr("foo", -1) should return ""')
+    assert(makeStr('foo', 0) === '', 'makeStr("foo", 0) should return ""')
 
-    assert(makeStr(['foo', 'bar'], 2) === 'foo,barfoo,bar', 'make_str(["foo", "bar"], 2) should return "foo,barfoo,bar"')
-    assert(makeStr(42, 2) === '4242', 'make_str(42, 2) should return "4242"')
-    assert(makeStr({}, 2) === '[object Object][object Object]', 'make_str({}, 2) should return "[object Object][object Object]"')
+    assert(makeStr(['foo', 'bar'], 2) === 'foo,barfoo,bar', 'makeStr(["foo", "bar"], 2) should return "foo,barfoo,bar"')
+    assert(makeStr(42, 2) === '4242', 'makeStr(42, 2) should return "4242"')
+    assert(makeStr({}, 2) === '[object Object][object Object]', 'makeStr({}, 2) should return "[object Object][object Object]"')
   })
 
-  test('cat function', function() {
+  test('cat function', () => {
     assert(cat() === '', 'cat() should return ""')
-    assert(cat('a',1,[2],{3:4}) === 'a12[object Object]', 'cat("a",1,[2],{3:4}) should return "a12[object Object]"')
+    assert(cat('a', 1, [2], {3: 4}) === 'a12[object Object]', 'cat("a",1,[2],{3:4}) should return "a12[object Object]"')
   })
 
-  test('capitalise function', function() {
+  test('capitalise function', () => {
     assert(capitalise('---the. first.') === '---The. first.', 'capitalise("---the. first.") should return "---The. first."')
     assert(capitalise('---the. first.', true) === '---The. First.', 'capitalise("---the. first.", true) should return "---The. First."')
     assert(capitalise('-Fo:b&ar. bAZ.', true) === '-Fo:B&Ar. Baz.', 'capitalise("-Fo:b&ar. bAZ.", true) should return "-Fo:B&Ar. Baz."')
   })
 
-  test('starts_withp function', function() {
-    assert(startsWithp('foobar', 'foo'), 'starts_
+  test('starts_withp function', () => {
+    assert(startsWithp('foobar', 'foo'), 'starts_withp("foobar", "foo") should return true')
+    assert(!startsWithp('foobar', 'bar'), "starts_withp('foobar', 'bar') should return false")
+  })
+
+  // Add tests for the remaining functions (endsWithp, hasp, count, dasherise, camelise)
+})
