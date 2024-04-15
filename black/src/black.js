971fefb7b09a27c2103fec74a3b44909cd50e588
Ítalo Gustavo <42496107+ItaloGustavoS@users.noreply.github.com>
2024-04-15 17:55:36 -0300

    improve

--- black/src/black.js
+++ black/src/black.js
@@ -1,2 +1,23 @@
-// ---------------------------------- black -----------------------------------
-// 
+#include <stdio.h>
+
+/**
+ * @brief A function to print the ASCII value of a character.
+ *
+ * @param ch The character whose ASCII value is to be printed.
+ */
+void printAsciiValue(char ch) {
+    printf("The ASCII value of '%c' is %d.\n", ch, (int)ch);
+}
+
+int main() {
+    // Print the ASCII value of the character 'a'
+    printAsciiValue('a');
+
+    // Print the ASCII value of the character 'B'
+    printAsciiValue('B');
+
+    // Print the ASCII value of the character '1'
+    printAsciiValue('1');
+
+    return 0;
+}
