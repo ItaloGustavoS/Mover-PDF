d5a0d753311c3dc2622fe0d827f8c8e6a74febf3
Ítalo Gustavo <42496107+ItaloGustavoS@users.noreply.github.com>
2024-04-15 17:55:40 -0300

    improve

--- black/src/fn.js
+++ black/src/fn.js
@@ -1,88 +1,95 @@
+'use strict';
+
 /******************************************************************************
- *                                    ~fn~                                    *
- *                                  ‾‾‾‾‾‾‾‾                                  *
- * Function utilities.                                                        *
+ * Function utilities.
  *     _________________________________________________________________      *
  *        Copyright (c) 2011 Quildreen Motta // Licenced under MIT/X11        *
  ******************************************************************************/
-void function (root) {
 
-    var fn
+function functionUtils(root) {
+  var functionUtil = {};
 
-    , slice = Array.prototype.slice
+  var slice = Array.prototype.slice;
 
+  ///// Function partial /////////////////////////////////////////////////////
+  //
+  // Returns a function that will always have the given arguments
+  // prepended to the argument list passed to the function.
+  //
+  function partial(fn) {
+    var args = slice.call(arguments, 1);
+    return function () {
+      return fn.apply(this, args.concat(slice.call(arguments)));
+    };
+  }
 
-    ///// Function partial /////////////////////////////////////////////////////
-    //
-    //   (fn:Fn, args...) ↦ Fn
-    //
-    // Return a function that'll always have the given arguments
-    // prepended to argument list passed to the function.
-    //
-    function partial(fn) {
-        var args = slice.call(arguments, 1)
-        return function() { return fn.apply(this, args.concat(slice.call(arguments))) }
-    }
+  ///// Function delay ///////////////////////////////////////////////////////
+  //
+  // Calls the function after the given number of seconds.
+  //
+  // Returns an ID to a timer, such that the call to the function can
+  // be cancelled by passing that ID to `clearTimeout`.
+  //
+  function delay(fn, seconds) {
+    var args = slice.call(arguments, 2);
+    if (args.length) fn = fn.bind.apply(fn, args);
 
+    return setTimeout(fn, seconds * 1000);
+  }
 
-    ///// Function delay ///////////////////////////////////////////////////////
-    //
-    //   (fn:Fn, seconds:Num) ↦ Num
-    //
-    // Calls the function after the given number of seconds.
-    //
-    // Returns an ID to a timer, such that the call to the function can
-    // be cancelled by passing that ID to `clearTimeout`.
-    //
-    function delay(fn, seconds) { var args
-        args = slice.call(arguments, 2)
-        if (args.length)  fn = fn.bind.apply(fn, args)
+  ///// Function defer ///////////////////////////////////////////////////////
+  //
+  // Same as `delay`, but calls the function as soon as the engine is
+  // not busy doing other things.
+  //
+  function defer(fn) {
+    var args = slice.call(arguments, 1);
+    return delay.apply(this, [fn, 0].concat(args));
+  }
 
-        return setTimeout(fn, seconds * 1000)
-    }
+  ///// Function wrap ////////////////////////////////////////////////////////
+  //
+  // Returns a function that passes the given `wrapper` as the first
+  // argument for `fn`.
+  //
+  function wrap(fn, wrapper) {
+    if (!wrapper)
+      wrapper = function (f) {
+        f.apply(this, slice.call(arguments, 1));
+      };
 
-    ///// Function defer ///////////////////////////////////////////////////////
-    //
-    //   (fn:Fn) ↦ Num
-    //
-    // Same as `delay`, but calls the function as soon as the engine is
-    // not busy doing other things.
-    //
-    function defer(fn) { var args
-        args = slice.call(arguments, 1)
-        return delay.apply(this, [fn, 0].concat(args))
-    }
+    return partial(wrapper, fn);
+  }
 
+  ///// Exports //////////////////////////////////////////////////////////////
+  functionUtil =
+    typeof exports === 'undefined'
+      ? (root.functionUtils = {})
+      : exports;
 
-    ///// Function wrap ////////////////////////////////////////////////////////
-    //
-    //   (fn:Fn[, wrapper:Fn]) ↦ Fn
-    //
-    // Returns a function that passes the given `wrapper` as the first
-    // argument for `fn`.
-    //
-    function wrap(fn, wrapper) {
-        if (!wrapper)
-            wrapper = function(f){ f.apply(this, slice.call(arguments, 1)) }
+  functionUtil.partial = partial;
+  functionUtil.delay = delay;
+  functionUtil.defer = defer;
+  functionUtil.wrap = wrap;
+}
 
-        return partial(wrapper, fn)
-    }
+// Demonstrate the usage of the functions
+functionUtils(this);
 
+var sum = function (a, b) {
+  return a + b;
+};
 
-
-    ///// Exports //////////////////////////////////////////////////////////////
-    fn = typeof exports == 'undefined'?  root.black.fn = { }
-       :                                 exports
+var addOne = function (x) {
+  return x + 1;
+};
 
-    fn.partial = partial
-    fn.delay   = delay
-    fn.defer   = defer
-    fn.wrap    = wrap
+var addOneDelayed = functionUtils.delay(addOne, 1000);
+console.log(addOneDelayed(5)); // Logs 6 after 1 second
 
-    fn.$black_box    = Function
-    fn.$black_proto  = Function.prototype
-    fn.$black_utils  = { partial : partial
-                       , delay   : delay
-                       , defer   : defer
-                       , wrap    : wrap }
-}(this)
+var addFive = functionUtils.wrap(sum, function (f) {
+  return function (a, b) {
+    return f(a + 5, b + 5);
+  };
+});
+console.log(addFive(2, 3)); // Logs 12
