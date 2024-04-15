669403537052cf31818e919734aa4aaee5088234
Ítalo Gustavo <42496107+ItaloGustavoS@users.noreply.github.com>
2024-04-15 17:55:46 -0300

    improve

--- black/src/type.js
+++ black/src/type.js
@@ -1,122 +1,80 @@
 /******************************************************************************
- *                                   ~type~                                   *
+ *                                   TypeCheck                                 *
  *                                 ‾‾‾‾‾‾‾‾‾‾                                 *
  * Type and functionality testing.                                            *
  *     _________________________________________________________________      *
  *        Copyright (c) 2011 Quildreen Motta // Licenced under MIT/X11        *
  ******************************************************************************/
 
-/// Module type ////////////////////////////////////////////////////////////////
-type Module = {
-  class_of: (obj: any) => string;
-  nilp: (obj: any) => boolean;
-  not_nilp: (obj: any) => boolean;
-  undefp: (obj: any) => boolean;
-  strp: (obj: any) => boolean;
-  nump: (obj: any) => boolean;
-  regexpp: (obj: any) => boolean;
-  fnp: (obj: any) => boolean;
-  boolp: (obj: any) => boolean;
-  objp: (obj: any) => boolean;
-  listp: (obj: any) => boolean;
-  callablep: (obj: any) => boolean;
-  numericp: (obj: any) => boolean;
-  sequencep: (obj: any) => boolean;
-  sliceablep: (obj: any) => boolean;
-  searchablep: (obj: any) => boolean;
-  testablep: (obj: any) => boolean;
-  $black_box: any;
-  $black_utils: {
-    class_of: (obj: any) => string;
-    nilp: (obj: any) => boolean;
-    not_nilp: (obj: any) => boolean;
-    undefp: (obj: any) => boolean;
-    strp: (obj: any) => boolean;
-    nump: (obj: any) => boolean;
-    regexpp: (obj: any) => boolean;
-    fnp: (obj: any) => boolean;
-    boolp: (obj: any) => boolean;
-    objp: (obj: any) => boolean;
-    listp: (obj: any) => boolean;
-    callablep: (obj: any) => boolean;
-    numericp: (obj: any) => boolean;
-    sliceablep: (obj: any) => boolean;
-    searchablep: (obj: any) => boolean;
-    testablep: (obj: any) => boolean;
-  };
+/// Type Checking Functions ////////////////////////////////////////////////////
+
+const classOf = (obj: any): string =>
+  Object.prototype.toString.call(obj).slice(8, -1);
+
+const isNil = (obj: any): boolean => obj == null;
+const isNotNil = (obj: any): boolean => obj != null;
+const isUndef = (obj: any): boolean => obj === void 0;
+const isStr = (obj: any): boolean => classOf(obj) === "String";
+const isNum = (obj: any): boolean => classOf(obj) === "Number";
+const isRegExp = (obj: any): boolean => classOf(obj) === "RegExp";
+const isFn = (obj: any): boolean => classOf(obj) === "Function";
+const isBool = (obj: any): boolean => classOf(obj) === "Boolean";
+const isObj = (obj: any): boolean => obj === Object(obj);
+const isList = (obj: any): boolean => obj instanceof Array;
+const isCallable = (obj: any): boolean => typeof obj === "function";
+const isNumeric = (obj: any): boolean => !isNaN(obj);
+const isSequence = (obj: any): boolean =>
+  isNotNil(obj) && typeof obj.length === "number";
+const isSliceable = (obj: any): boolean =>
+  isNotNil(obj) && typeof obj.slice === "function";
+const isSearchable = (obj: any): boolean =>
+  isNotNil(obj) &&
+  typeof obj.indexOf === "function" &&
+  typeof obj.lastIndexOf === "function";
+const isTestable = (obj: any): boolean =>
+  isNotNil(obj) && typeof obj.test === "function";
+
+/// TypeCheck Module ///////////////////////////////////////////////////////////
+
+type TypeCheck = {
+  classOf: typeof classOf;
+  isNil: typeof isNil;
+  isNotNil: typeof isNotNil;
+  isUndef: typeof isUndef;
+  isStr: typeof isStr;
+  isNum: typeof isNum;
+  isRegExp: typeof isRegExp;
+  isFn: typeof isFn;
+  isBool: typeof isBool;
+  isObj: typeof isObj;
+  isList: typeof isList;
+  isCallable: typeof isCallable;
+  isNumeric: typeof isNumeric;
+  isSequence: typeof isSequence;
+  isSliceable: typeof isSliceable;
+  isSearchable: typeof isSearchable;
+  isTestable: typeof isTestable;
 };
 
-function class_of(obj: any): string {
-  return Object.prototype.toString.call(obj).slice(8, -1);
-}
-
-function nilp(obj: any): boolean {
-  return obj == null;
-}
-
-function not_nilp(obj: any): boolean {
-  return obj != null;
-}
-
-function undefp(obj: any): boolean {
-  return obj === void 0;
-}
-
-function strp(obj: any): boolean {
-  return class_of(obj) === "String";
-}
-
-function nump(obj: any): boolean {
-  return class_of(obj) === "Number";
-}
-
-function regexpp(obj: any): boolean {
-  return class_of(obj) === "RegExp";
-}
-
-function fnp(obj: any): boolean {
-  return class_of(obj) === "Function";
-}
-
-function boolp(obj: any): boolean {
-  return class_of(obj) === "Boolean";
-}
-
-function objp(obj: any): boolean {
-  return obj === Object(obj);
-}
-
-function listp(obj: any): boolean {
-  return obj instanceof Array;
-}
-
-function callablep(obj: any): boolean {
-  return typeof obj === "function";
-}
-
-function numericp(obj: any): boolean {
-  return !isNaN(obj);
-}
-
-function sequencep(obj: any): boolean {
-  return not_nilp(obj) && typeof obj.length === "number";
-}
-
-function sliceablep(obj: any): boolean {
-  return not_nilp(obj) && typeof obj.slice === "function";
-}
-
-function searchablep(obj: any): boolean {
-  return (
-    not_nilp(obj) &&
-    typeof obj.indexOf === "function" &&
-    typeof obj.lastIndexOf === "function"
-  );
-}
-
-function testablep(obj: any): boolean {
-  return not_nilp(obj) && typeof obj.test === "function";
-}
+const typeCheck: TypeCheck = {
+  classOf,
+  isNil,
+  isNotNil,
+  isUndef,
+  isStr,
+  isNum,
+  isRegExp,
+  isFn,
+  isBool,
+  isObj,
+  isList,
+  isCallable,
+  isNumeric,
+  isSequence,
+  isSliceable,
+  isSearchable,
+  isTestable,
+};
 
 /// Exports //////////////////////////////////////////////////////////////
 const root =
@@ -128,26 +86,9 @@ const root =
     ? window
     : {};
 
-let type: Module;
+let typeCheckModule: TypeCheck;
 if (typeof exports === "undefined") {
-  type = root.black.type;
+  typeCheckModule = root.typeCheck = typeCheck;
 } else {
-  type = exports;
+  typeCheckModule = exports;
 }
-
-type.class_of = class_of;
-type.nilp = nilp;
-type.not_nilp = not_nilp;
-type.undefp = undefp;
-type.strp = strp;
-type.nump = nump;
-type.regexpp = regexpp;
-type.fnp = fnp;
-type.boolp = boolp;
-type.objp = objp;
-type.listp = listp;
-type.callablep = callablep;
-type.numericp = numericp;
-type.sequencep = sequencep;
-type.sliceablep = sliceablep;
-type
